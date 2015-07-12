f567c29c00c7ffbe877a5d3f2d504a0bc7ec18d7
Patrick Freeman <pfreeman@Patricks-MacBook-Pro.local>
2015-07-11 22:14:37 -0400

    restructoring folders

---
+++ elixir/recipes/elixir.rb
@@ -0,0 +1,56 @@
+# vars
+install_dir = "#{node[:elixir][:art_dir]}"
+erlang_source = "#{node[:elixir][:elang_source]}"
+elixr_source = "#{node[:elixir][:elixr_source]}"
+
+# install unzip package
+%w{unzip}.each do |pak|
+  package "#{pak}" do 
+    action :install
+  end
+end
+
+# create erlang and elixir dirs
+%w{elixir erlang}.each do |dir|
+  directory "#{install_dir}/#{dir}" do
+    mode '0755'
+    owner 'root'
+    group 'root'
+    action :create
+    recursive true
+  end
+end
+
+# download erlang 
+remote_file "#{install_dir}/erlang/esl-erlang.rpm" do
+  source "#{erlang_source}"
+  mode 0755
+end
+
+# install erlang
+package "esl-erlang" do
+  source "#{install_dir}/erlang/esl-erlang.rpm"
+  action :install
+end
+
+# elixir
+remote_file "#{install_dir}/elixir/elixir.zip" do
+  source "#{elixr_source}"
+  mode 0755
+  notifies :run, "execute[elixir-zip]", :immediately
+end
+
+# install elixir
+execute "elixir-zip" do
+  command "unzip #{install_dir}/elixir/elixir.zip"
+  action :nothing
+end
+
+# create elixir system-wide environment viriables
+bash "elixir-profiled" do
+  code  <<-EOH
+  echo "export PATH="$PATH:#{install_dir}/bin"" > /etc/profile.d/elixir.sh
+  EOH
+  not_if {File.exist?("/etc/profile.d/elixir.sh") }
+end
+
